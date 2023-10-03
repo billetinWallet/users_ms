7b9e762a9d30d1f708ce7509a025202d8ce779cf
Nerón <54643791+Slopezpa@users.noreply.github.com>
2023-10-02 22:18:08 -0600

    Update model_user.js

--- user_ms/models/model_user.js
+++ user_ms/models/model_user.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
   first_name: String,
   last_name: String,
   sex: String,
-  verified: Boolean,
+  verified: Boolean
 });
 
 module.exports = mongoose.model('User', userSchema);
